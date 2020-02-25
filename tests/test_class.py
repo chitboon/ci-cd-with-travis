c494d13c275c85a1449d1c1fce55f2c666c401c3
Lee Chit Boon <chitboon@gmail.com>
2020-02-26 07:57:34 +0800

    Updated with Proc file

--- tests/test_class.py
+++ tests/test_class.py
@@ -11,7 +11,7 @@ class TestAskMe(unittest.TestCase):
         pass
 
     def test_page(self):
-        response = self.app.get('/askme', follow_redirects=True)
+        response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
     
     def test_num_docs(self):
